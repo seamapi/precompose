15c1c7ba2a0831c8857045e4f32fc5b28ca03c8f
Jordan Webb <jordan@getseam.com>
2021-04-27 14:31:28 -0500

    Migrate to setup.cfg

--- setup.py
+++ setup.py
@@ -1,16 +1,5 @@
-# type: ignore
+from setuptools import setup
 
-import setuptools
-
-setuptools.setup(
-    name="holocron",
-    version="0.1.0",
-    author="Seam",
-    author_email="hello@getseam.com",
-    description="holocron imports Docker Compose applications into ostree",
-    url="https://github.com/hello-seam/holocron",
-    packages=["holocron"],
-    python_requires=">=3.8",
-    license="AGPL-3.0-or-later",
-    entry_points={"console_scripts": ["holocron=holocron.main:main"]},
+setup(
+    use_scm_version={"local_scheme": "no-local-version"},
 )
